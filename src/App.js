30d85017df28361f8a981bce45afcb493e71619b
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-18 18:02:51 +0200

    Import SemanticUI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Container, Header, Statistic } from 'semantic-ui-react'
 import './App.css'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
-    return <div className="App">Hello World</div>
+    return (
+        <Container>
+            <Header as="h1">Budget</Header>
+            <Statistic size="small">
+                <Statistic.Label>Your balance:</Statistic.Label>
+                <Statistic.Value>2,500.53</Statistic.Value>
+            </Statistic>
+        </Container>
+    )
 }
 
 export default App
