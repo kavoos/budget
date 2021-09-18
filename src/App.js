602212dcb2068987764746cc977f89332a1833c8
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-18 18:40:28 +0200

    Create income and expense history

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
-import { Container, Grid, Header, Segment, Statistic } from 'semantic-ui-react'
+import {
+    Container,
+    Grid,
+    Header,
+    Icon,
+    Segment,
+    Statistic,
+} from 'semantic-ui-react'
 import './App.css'
 import 'semantic-ui-css/semantic.min.css'
 
@@ -6,10 +13,12 @@ function App() {
     return (
         <Container>
             <Header as="h1">Budget</Header>
+
             <Statistic size="small">
                 <Statistic.Label>Your balance:</Statistic.Label>
                 <Statistic.Value>2,500.53</Statistic.Value>
             </Statistic>
+
             <Segment textAlign="center">
                 <Grid columns={2} divided>
                     <Grid.Row>
@@ -28,6 +37,42 @@ function App() {
                                 </Statistic.Label>
                                 <Statistic.Value>892.54</Statistic.Value>
                             </Statistic>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+
+            <Header as="h3">History</Header>
+
+            <Segment color="red">
+                <Grid columns={3} textAlign="right">
+                    <Grid.Row>
+                        <Grid.Column width={10} textAlign="left">
+                            Restaurant
+                        </Grid.Column>
+                        <Grid.Column width={3} textAlign="right">
+                            €120.99
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Icon name="edit" />
+                            <Icon name="trash" />
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
+
+            <Segment color="green">
+                <Grid columns={3} textAlign="right">
+                    <Grid.Row>
+                        <Grid.Column width={10} textAlign="left">
+                            Restaurant
+                        </Grid.Column>
+                        <Grid.Column width={3} textAlign="right">
+                            €120.99
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                            <Icon name="edit" />
+                            <Icon name="trash" />
                         </Grid.Column>
                     </Grid.Row>
                 </Grid>
