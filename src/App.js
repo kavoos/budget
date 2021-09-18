fca925a9b6cfcd2be92c72fee0b7f41936d38cf0
Kavoos Boloorchi <kavoos.boloorchi@gmail.com>
2021-09-18 18:14:18 +0200

    Add income and expenses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container, Header, Statistic } from 'semantic-ui-react'
+import { Container, Grid, Header, Segment, Statistic } from 'semantic-ui-react'
 import './App.css'
 import 'semantic-ui-css/semantic.min.css'
 
@@ -10,6 +10,28 @@ function App() {
                 <Statistic.Label>Your balance:</Statistic.Label>
                 <Statistic.Value>2,500.53</Statistic.Value>
             </Statistic>
+            <Segment textAlign="center">
+                <Grid columns={2} divided>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Statistic size="tiny" color="green">
+                                <Statistic.Label style={{ textAlign: 'left' }}>
+                                    Incoming:
+                                </Statistic.Label>
+                                <Statistic.Value>1,500.54</Statistic.Value>
+                            </Statistic>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Statistic size="tiny" color="red">
+                                <Statistic.Label style={{ textAlign: 'left' }}>
+                                    Expenses:
+                                </Statistic.Label>
+                                <Statistic.Value>892.54</Statistic.Value>
+                            </Statistic>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Segment>
         </Container>
     )
 }
